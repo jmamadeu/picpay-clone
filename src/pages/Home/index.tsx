5772273bc244e1ab4914d835a2841f7ae4ab3ef9
João Amadeu <jmamadeu2000@gmail.com>
2020-04-30 07:47:31 +0100

    Importação dos componentes para término da construção da página

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
 
-import { Text } from 'react-native';
+import Suggestions from '../../components/Suggestions';
+import Activities from '../../components/Activities';
+import Tips from '../../components/Tips';
+import Banner from '../../components/Banner';
+
+import {
+  Container,
+  Wrapper,
+  Header,
+  BalanceContainer,
+  BalanceTitle,
+  Balance,
+} from './styles';
 
 const Home: React.FC = () => {
-  return <Text>Home</Text>;
+  return (
+    <Wrapper>
+      <Container>
+        <Header>
+          <MaterialCommunityIcons
+            name='qrcode-scan'
+            size={30}
+            color='#10c86e'
+          />
+          <BalanceContainer>
+            <BalanceTitle>Meu Saldo</BalanceTitle>
+            <Balance>AKZ 980.900.90,00</Balance>
+          </BalanceContainer>
+
+          <AntDesign name='gift' color='#10c86e' size={30} />
+        </Header>
+
+        <Suggestions />
+        <Activities />
+        <Tips />
+        <Banner />
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Home;
